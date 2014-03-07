e94f39a04455ff1f765ca1ef05570b4df55e1e7a
Aliya Rahman <aliya.rahman@yahoo.com>
2014-03-07 01:54:15 -0500

    Updated settings

--- possibiliT/settings.py
+++ possibiliT/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for possibiliT project.
+Django settings for gaytabase project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -10,14 +10,16 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import psycopg2
+import urlparse
+from urlparse import urlparse
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gu3i0j06b*xx$el(2j*yatzegb&@i^0gilt54rvr_e3k1jfe11'
+SECRET_KEY = 'j!h0x=i!_)_d52pwbrzhou+6e&c4r4^7+7zayqc(0=kzo#%cp+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,16 +54,13 @@ ROOT_URLCONF = 'possibiliT.urls'
 
 WSGI_APPLICATION = 'possibiliT.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -79,4 +79,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = '' # This is where you want the collectstatic app to put all of the files for deployment, when it pulls from multiple DIRS
+STATIC_URL = '/static/' #This is the part that comes before what you are using in {% static %} tags to reference files
+STATIC_DIRS = '/home/aliya/gitrepos/possibiliT/app/static/app/' #These are where the files are actually stored - this is really important for deployment on servers like Bluehost where you can't save inside your project directory
+
+
+TEMPLATE_DIRS = (
+	'/home/aliya/gitrepos/possibiliT/app/templates/app/',
+	'app/templates/app/',
+)
+
+LOGIN_URL = 'django.contrib.auth.views.login'
+LOGIN_REDIRECT_URL = 'index'
+
+AUTH_USER_MODEL = 'auth.User'
